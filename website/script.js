7ad7eb379935252b5390a351c6528a003c05af34
Thomas Koji Abe <22721346+thomaskoji@users.noreply.github.com>
2025-04-09 21:41:34 +0100

    update

--- website/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("keydown", function(event)
-{
-    if (event.key === "d" || event.key === "D") {
-        document.body.classList.toggle("debug");
-        console.log("hi");
-    }
-});
